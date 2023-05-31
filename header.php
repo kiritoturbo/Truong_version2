5fe9e42b9f927c58edd0f8a38cb961b4c4b3a90b
kirito <manhtruong@gmail.com>
2023-05-31 09:12:05 +0700

    commit

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php bloginfo('name'); ?></title>
-    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/style.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/styles.css" />
     <?php wp_head(); ?>
 </head>
 
